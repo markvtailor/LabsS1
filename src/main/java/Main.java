152c74f72e9ac4cb072ff5057d0cab5df4be1cdd
Mark Tailor <markvtailor@gmail.com>
2022-01-13 09:58:52 +0300

    Лаб2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        double sum = 0;
+        for (String num: args){
+            try{
+                double polyn = Double.parseDouble(num);
+                sum += (1/polyn)*27;
+            }catch (NumberFormatException e){
+                num = null;
+            }
+
+        }
+        System.out.println(sum);
+    }
 }
